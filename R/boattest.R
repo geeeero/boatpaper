b8d6f6a3a6b6f973b489fb9138c559397221171c
Gero Walter <gero.walter@stat.uni-muenchen.de>
2014-08-26 17:16:50 +0200

    redraw spda proof graph, rewrite explaining paragraph

--- R/boattest.R
+++ R/boattest.R
@@ -7,6 +7,7 @@ source("boatfunctions.R")
 #install.packages("luck", repos="http://R-Forge.R-project.org")
 library("luck")
 
+setEPS()
 
 # display the domain \Eta
 postscript("boatshape-domain.ps", width=5, height=5)
@@ -71,6 +72,54 @@ boat1$data <- list(tau = 16, n = 16)
 normalplotter(boat1, prior=F, add=T)
 dev.off()
 
+# plot for strong prior-data agreement reaction
+expoprior     <- function(eta, etal, b, n = NULL) exp(b*(eta-etal))
+expoposterior <- function(eta, etal, b, n)        exp(b*(eta-etal-n))
+leftside      <- function(eta, b)                 1 + b*(eta + 2)
+
+
+etavec <- seq(0,15,by=0.01)
+etal <- 2
+b <- 0.25
+n <- 5
+
+eta0finder <- function(expfu, linfu, etal, b, n, rootinterval = c(-2, 100)){
+  rootfu <- function(eta){
+    expres <- do.call(expfu, list(eta=eta, etal=etal, b=b, n=n)) 
+    linres <- do.call(linfu, list(eta=eta, b=b))
+    expres - linres
+  }
+  res <- uniroot(rootfu, rootinterval)
+  list(x=res$root, y=do.call(linfu, list(eta=res$root, b=b)))  
+}
+
+par(mar=c(5,4,1,1)+0.1)
+#pdf("prior-vs-posterior-eta0u.pdf", width=8, height=6)
+postscript("prior-vs-posterior-eta0u.eps", width=8, height=5)
+plot(etavec, expoprior(etavec,etal,b), ylim=c(-1,8), type="l",
+     yaxp=c(0,8,8), 
+     xlab=expression(eta[0]), ylab="tangent equation sides")
+lines(etavec, expoposterior(etavec,etal,b,n))
+lines(etavec, leftside(etavec,b))
+#abline(1+2*b,b)
+#abline(v=c(etal,etal+n))
+abline(h=0)
+eta0u0 <- eta0finder(expoprior, leftside, etal, b, n)
+eta0un <- eta0finder(expoposterior, leftside, etal, b, n)
+eta0u0pn <- eta0finder(expoposterior, function(eta, b) eta0u0$y, etal, b, n)
+points(eta0u0)
+points(eta0un)
+points(eta0u0pn)
+lines(rep(eta0u0$x,2), c(eta0u0$y,-0.1), lty=2)
+lines(rep(eta0un$x,2), c(eta0un$y,-0.1), lty=2)
+lines( c(eta0u0$x, rep(eta0u0pn$x, 2)), c(rep(eta0u0$y, 2), -0.1), lty=2)
+text(x=c(eta0u0$x, eta0un$x, eta0u0pn$x), y=rep(-0.1,3),
+     labels=c(expression(eta[0]^u(0)),
+              expression(eta[0]^u(n)),
+              expression(eta[0]^u(0) + n)), pos=1)
+dev.off()
+par(mar=c(5,4,4,2)+0.1)
+#
 
 
 
