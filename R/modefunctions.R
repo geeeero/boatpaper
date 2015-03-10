6359d5c3b7c5e4dfa950aaebee4216f9777be382
Gero Walter <g.m.walter@tue.nl>
2015-03-10 15:51:55 +0100

    beta mode exception handling

--- R/modefunctions.R
+++ R/modefunctions.R
@@ -1,7 +1,19 @@
 # functions for the mode as posterior inference of interest
 
-# mode as function of n0 and y0 (where n > 2, 1 < n*y < n-1)
-betamode <- function(n,y) (n*y - 1)/(n-2)
+# mode as function of n0 and y0 (where n   > 2, 1 < n*y < n-1)
+#                          (i.e. where a+b > 2, 1 < a   < a+b-1)
+betamode <- function(n, y, trunkate = TRUE){
+  # beware of numerical problems!
+  # testing if n <= 2
+  if(isTRUE(all.equal(n, 2)) || n <= 2)
+    stop(paste("No proper mode for n <= 2 (n = ", n, ")", sep=""))
+  res <- (n*y - 1)/(n-2)
+  if(trunkate){
+    if(res < 0) res <- 0
+    if(res > 1) res <- 1 
+  }
+  res
+}
 
 # plot region where mode is defined in normal world and miksworld
 # mode undefined for priors with xp[1] < 0
@@ -11,16 +23,17 @@ betamode <- function(n,y) (n*y - 1)/(n-2)
 # we will find the extremes on the lower and upper contours
 
 # returns vector of extreme mode values for given vector of s values (s in [0, n])
-modefinder <- function(boatobj, svec, lower = TRUE, seqx = 200, prior = FALSE){
+smodefinder <- function(boatobj, svec, lower = TRUE, seqx = 200, prior = FALSE){
   if(lower){
     wh <- -1
   } else {
     wh <- 1
   }
   res <- rep(0, length(svec))
+  sboat <- boatobj
   for (i in 1:length(svec)){
-    boatobj$data$tau <- svec[i]
-    mikseq <- boatfu(boatobj = boatobj, wh =  wh, xlen = seqx, fw = TRUE,  prior = prior)
+    sboat$data$tau <- svec[i]
+    mikseq <- boatfu(boatobj = sboat, wh =  wh, xlen = seqx, fw = TRUE,  prior = prior)
     normalseq <- miktonormal(mikseq) # returns list(x=n0,y=y0)
     if(lower){
       res[i] <- min(betamode(normalseq$x,normalseq$y))
@@ -31,21 +44,28 @@ modefinder <- function(boatobj, svec, lower = TRUE, seqx = 200, prior = FALSE){
   res
 }
 
-priormodefinder <- function(boatobj){
-  #boatobj$data <- list(tau=0, n=0)
-  #lmode <- modefinder(priorboatobj, svec=c(0))
-  #umode <- modefinder(priorboatobj, svec=c(0), lower = FALSE)
+modefinder <- function(boatobj, seqx = 200, prior = TRUE){
+  lowermik<- boatfu(boatobj = boatobj, wh = -1, xlen = seqx, fw = FALSE,  prior = prior)
+  uppermik <- boatfu(boatobj = boatobj, wh =  1, xlen = seqx, fw = FALSE,  prior = prior)
+  lowernorm <- miktonormal(lowermik) # returns list(x=n0,y=y0)
+  uppernorm <- miktonormal(uppermik) # returns list(x=n0,y=y0)
+  lmode <- betamode(lowernorm$x, lowernorm$y)
+  umode <- modefinder(priorboatobj, svec=c(0), lower = FALSE)
   
 }
 
-modeplotter <- function(boatobj, pppn, svecby = 0.01){
+
+modeplotter <- function(boatobj,, prior = FALSE)
+
+
+pppmodeplotter <- function(boatobj, pppn, svecby = 0.01){
   par(mfrow=c(1,2), mar=c(5,4.5,4,1)+0.1)
   # prior set
   normalplotter(boatobj, main="Prior parameter set")
   # varying s plot
   svec <- seq(0, pppn, by=svecby)
-  lvec <- modefinder(boatobj = boatobj, svec = svec)
-  uvec <- modefinder(boatobj = boatobj, svec = svec, lower = FALSE)
+  lvec <- smodefinder(boatobj = boatobj, svec = svec)
+  uvec <- smodefinder(boatobj = boatobj, svec = svec, lower = FALSE)
   plot(svec, uvec, type="l", ylim=c(0,1), xlab="s", ylab="mode",
        main = bquote(paste("Posterior imprecision (n=",.(pppn),")")))
   lines(svec, lvec, type="l")
